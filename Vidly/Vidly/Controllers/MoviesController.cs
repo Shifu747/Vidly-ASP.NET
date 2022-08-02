b9ccb4893c1d2386ce17467c53b2c32ca15a06a4
Shifu747 <shify747@gmail.com>
2022-08-02 14:55:27 +0600

    files added

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        private IEnumerable<Movies> GetMovies()
+        {
+            return new List<Movies>
+            {
+                new Movies { Id = 1, Name = "Shrek"},
+                new Movies { Id = 2, Name = "Wall-e"}
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+        }
+
+    }
+}

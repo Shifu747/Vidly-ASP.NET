4ba8ea24c4b23376cfdb9d95149d89138b51dc43
Shifu747 <shify747@gmail.com>
2022-08-03 16:50:59 +0600

    working with data lesson

--- Vidly-2.0/Vidly-2.0/Controllers/CustomersController.cs
+++ Vidly-2.0/Vidly-2.0/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,34 +10,38 @@ namespace Vidly_2._0.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context; //to access db
+
+        public CustomersController() //initialize constructor
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //dbcontext is disposable object . disposing
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
 
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            //.ToList executes the query
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
         // GET: Customers/Details/{id}
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
                 return HttpNotFound();
             }
             return View(customer);
-        }
-
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "John Smith"},
-                new Customer {Id = 2, Name = "Marry Williams"}
-            };
         }
 
     }

e713bb1ba77436917d9ee20582d4d009a854c273
Raisul Bari Shifat <91802334+Shifu747@users.noreply.github.com>
2022-08-22 15:17:36 +0600

    Building Forms lectures

---
+++ Vidly-2.0/Vidly-2.0/viewModels/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_2._0.Models;
+
+namespace Vidly_2._0.viewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
